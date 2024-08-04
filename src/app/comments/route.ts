7add322e6c225e09f86f1aa31f0ecfb4dc43cce1
Matthieu <matthieu.degny.dev@gmail.com>
2024-08-04 18:30:43 +0200

    handle header request

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,6 +1,7 @@
 import { type NextRequest } from "next/server";
 import { comments } from "./data";
 
+// route handler for GET /comments
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const query = searchParams.get("query");
@@ -10,7 +11,7 @@ export async function GET(request: NextRequest) {
   return Response.json(filteredComments);
 }
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const comment = await request.json();
   const newComment = { id: comments.length + 1, text: comment.text };
   comments.push(newComment);
